fb7670b0b95fa7b19f953e93b5041c248b742f73
dinhthikieu1405-max <dinhthikieu1405@gmail.com>
2026-08-28 16:40:39 +0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://objynjuaetgaqcwnetri.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_9J0SKKbCY-5Mr0XbCW8cuA_xB5C3lIX";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
